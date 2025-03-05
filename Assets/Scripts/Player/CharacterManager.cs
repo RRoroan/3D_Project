f1a5af5a025ea86a6de3c90942a98c4a1b44a8f3
RRoroan <tjdals8788@gmail.com>
2025-03-05 19:37:35 +0900

    [Feat] CharacterManager Script
    Singleton CharacterManger

--- Assets/Scripts/Player/CharacterManager.cs
+++ Assets/Scripts/Player/CharacterManager.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
+    private static CharacterManager instance;
+    public static CharacterManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("CharacterManager").AddComponent<CharacterManager>();
+            }
+            return instance;
+        }
+    }
+
+    public Player _player;
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance == this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
